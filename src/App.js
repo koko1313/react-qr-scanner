f2803299ebfc4f932dff5447f779ff02f000c116
koko1313 <kaloyanvellichkov@gmail.com>
2021-05-29 12:40:39 +0300

    adding props.children to the component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import MyComponent from "./components/MyComponent";
 function App() {
   return (
     <>
-      <MyComponent title="Title1" placeholder="place 1"></MyComponent>
-      <MyComponent title="Title2" placeholder="place 2"></MyComponent>
+      <MyComponent title="Title1" placeholder="place 1">
+        This is my first component
+      </MyComponent>
+      <MyComponent title="Title2" placeholder="place 2">
+        This is my second component bla bla ...
+      </MyComponent>
     </>
   );
 }
--- src/components/MyComponent/index.js
+++ src/components/MyComponent/index.js
@@ -4,6 +4,7 @@ export default function MyComponent(props) {
     return (
         <div className="MyComponent">
             <h1>{props.title}</h1>
+            <p>{props.children}</p>
             <input type="text" placeholder={props.placeholder}/>
             <button>Push me</button>
         </div>
